1c67c7c3e2276682cdf1ab54b6d6ee9a51b09e54
PFinal_南丞 <1370288056@qq.com>
2024-11-18 13:48:17 +0800

    更新

--- tests/Chat/ChatTest.php
+++ tests/Chat/ChatTest.php
@@ -14,63 +14,59 @@ use PHPUnit\Framework\Attributes\Group;
 
 class ChatTest extends TestCase
 {
-//    /**
-//     * test_chat_st
-//     * @return void
-//     * @throws \CozeSdk\Kernel\Exception\HttpException
-//     */
-//    #[Group("chat_build")]
-//    public function test_chat_st()
-//    {
-//        $chat = new Chat($this->app);
-//        $responseClosure = $chat->setUserId("12345678")->Query("你好,用PHP写一个循环")->Build();
-//        var_dump($responseClosure);
-//        $this->assertIsArray($responseClosure);
-////        ob_start();
-////        $responseClosure = $chat->setBotId("7388055232066617394")->setUserId("123456789")->Query("你好,写一个首关于风景的诗词")->Build();
-////        $responseClosure();
-////        $output = ob_get_clean();
-////        var_dump($output);
-////        $this->assertIsString($output);
-//        //        $this->assertIsObject($res);
-//        //$this->assertIsArray($res);
-////        $chatId = $chat->getChatId();
-////        # var_dump($chatId);
-////        $this->assertIsString($chatId);
-//
-//
-//    }
+    /**
+     * test_chat_st
+     * @return void
+     * @throws \CozeSdk\Kernel\Exception\HttpException
+     */
+    #[Group("chat_build")]
+    public function test_chat_st()
+    {
+        $chat = new Chat($this->app);
+        $responseClosure = $chat->setUserId("12345678")->Query("你好,用PHP写一个循环")->Build();
+        var_dump($responseClosure);
+        $this->assertIsArray($responseClosure);
+    }
 //
     /**
      * @throws \CozeSdk\Kernel\Exception\HttpException
      */
-//    #[Group('chat_status')]
-//    public function test_get_chat_status()
-//    {
-//        $chat = new Chat($this->app);
-//        $message_info = $chat->setConversationId("7437376554340024358")->getChatRetrieve("7437376554340040742");
-//		var_dump($message_info);
-//        $this->assertIsArray($message_info);
-//    }
-//
-    /**
-     * @throws \CozeSdk\Kernel\Exception\HttpException
-     */
-//    #[Group("chat_message_list")]
-//    public function test_get_chat_message_list()
-//    {
-//        $chat = new Chat($this->app);
-//        $message_list = $chat->setConversationId("7437376554340024358")->getChatMessageList("7437376554340040742");
-//        var_dump($message_list);
-//        $this->assertIsArray($message_list);
-//    }
-//
+    #[Group('chat_status')]
+    public function test_get_chat_status()
+    {
+        $chat = new Chat($this->app);
+        $message_info = $chat->setConversationId("7437376554340024358")->getChatRetrieve("7437376554340040742");
+		var_dump($message_info);
+        $this->assertIsArray($message_info);
+    }
 
+    /**
+     * @throws \CozeSdk\Kernel\Exception\HttpException
+     */
+    #[Group("chat_message_list")]
+    public function test_get_chat_message_list()
+    {
+        $chat = new Chat($this->app);
+        $message_list = $chat->setConversationId("7437376554340024358")->getChatMessageList("7437376554340040742");
+        var_dump($message_list);
+        $this->assertIsArray($message_list);
+    }
+
+	/**
+	 * 测试发送消息的功能。
+	 * 这个测试方法创建了一个新的 `Chat` 类的实例，并通过传递 `$this->app` 参数来调用其构造函数。
+	 * 然后，它调用 `Chat` 实例的 `sendMessage` 方法，并传递一个中文消息（"PHP 接口是什么？"）。
+	 * 最后，它将 `sendMessage` 的响应打印为 JSON 编码的字符串，并使用 `assertIsArray` 方法断言响应是一个数组。
+	 * @return void
+	 * @throws \CozeSdk\Kernel\Exception\HttpException
+	 */
 	#[Group("chat_send_message")]
 	public function test_send_message()
 	{
 		$chat = new Chat($this->app);
 		$message_info = $chat->sendMessage("PHP 接口是什么?");
+		print_r(json_encode($message_info, JSON_UNESCAPED_UNICODE).PHP_EOL);
 		$this->assertIsArray($message_info);
 	}
+
 }
